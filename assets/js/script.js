1a1df66fc0daba82dd0d469068b5a935a913c718
Marcelo Diament <marcelodiament@gmail.com>
2020-04-25 11:21:07 -0300

    returning wikipedia API results in interactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ window.onload = () => {
 
     let delay = 500;
 
-    const interaction = (question, info, next) => {
+    const interaction = (question, info, next, middleFunction = null) => {
         setTimeout(() => {
             historico.append(`
                         <li class="bot">${question}</li>
@@ -18,12 +18,37 @@ window.onload = () => {
             historico.append(`
                         <li class="user">${respostas[`${info}`]}</li>
                     `);
-            if(respostas[`${info}`]) next();
+            if (respostas[`${info}`]) {
+                if (middleFunction !== null && middleFunction !== undefined) {
+                    middleFunction();
+                }
+                next();
+            }
         }, delay);
-        delay += 500;
-        console.log(respostas);
         return respostas;
     }
+
+    const buscar = termo => {
+        $('#historico').append(`
+            <li class="bot">
+                <h3>Algumas curiosidades sobre ${termo}</h3>
+        `);
+        fetch(`https://pt.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${termo}`)
+            .then(response => response.json())
+            .then(data => {
+                const resultados = data.query.search;
+                for (resultado of resultados) {
+                    $('#historico').append(`
+                        <h4>${resultado.title}</h4>\n
+                        <p>${resultado.snippet}</p>
+                    `);
+                }
+            })
+            .catch(() => {
+                console.log('Algo de errado não deu certo...')
+            })
+        $('#historico').append(`</li>`);
+    };
 
     const initialInteraction = () => {
         historico.append(`
@@ -41,7 +66,26 @@ window.onload = () => {
 
     };
 
-    const secondInteraction = () => interaction(`E quantos anos você tem ${respostas['nome']}?`, 'idade');
+    const secondInteraction = () => interaction(`E quantos anos você tem ${respostas['nome']}?`, 'idade', thirdInteraction);
+
+    const thirdInteraction = () => interaction(`${respostas['nome']}, você já fez aniversário esse ano?`, 'aniversariou', fourthInteraction, anoNascimento);
+
+    const fourthInteraction = () => interaction(`Hmmm... então você nasceu em ${respostas['anoNascimento']}!\nQual seu estilo musical preferido?`, 'preferenciaMusical', fifthInteraction);
+
+    const fifthInteraction = () => buscar(`${respostas['preferenciaMusical']} em ${respostas['anoNascimento']}`);
+
+    const anoNascimento = () => {
+        const anoAtual = new Date().getFullYear(),
+            idade = respostas['idade'],
+            aniversariou = respostas['aniversariou'];
+        let anoNascimento;
+        anoNascimento = aniversariou.toLowerCase() === 'sim' ? anoAtual - idade : anoAtual - idade - 1;
+        respostas['anoNascimento'] = anoNascimento;
+        return anoNascimento;
+    }
+
+
+
 
     init();
 
